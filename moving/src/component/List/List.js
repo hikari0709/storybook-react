ac9e5f5f93980c0d1f9fedf20cbbef080431abe7
hikari <tanimoto0709@gmail.com>
2023-04-28 06:42:49 +0900

    update: liの要素を配列で渡せるように変更

--- moving/src/component/List/List.js
+++ moving/src/component/List/List.js
@@ -5,14 +5,16 @@ function ListHeading({ text }) {
   return <p class="c-list__heading">{text}</p>;
 }
 
-function List({ heading, text }) {
+function List({ heading, text, items }) {
+  const listItems = items.map((item, index) => {
+    return <li key={index}>{item}</li>;
+  });
+
   return (
     <section class="p-article">
       {heading && <ListHeading text={text} />}
       <ul class="c-list">
-        <li>Item1</li>
-        <li>Item2</li>
-        <li>Item3</li>
+        {listItems}
       </ul>
     </section>
   );
--- moving/src/component/List/List.stories.js
+++ moving/src/component/List/List.stories.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import List from './List';
+import PropType from 'prop-types';
 
 export default {
   component: List,
   title: 'List',
   argTypes: {
     heading: {
-      control: { type: 'boolean' },
+      control: { type: PropType.boolean },
     },
     text: {
-      control: { type: 'text' },
+      control: { type: PropType.text },
     },
+    items: { type: PropType.array },
   },
 };
 
@@ -21,4 +23,5 @@ export const Default = Template.bind({});
 Default.args = {
   heading: false,
   text: 'リストのタイトルが入ります',
+  items: ['item1', 'item2', 'item3'],
 };
